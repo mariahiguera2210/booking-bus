170fe1d35dd07237662ce6d1f38982de0dfe9323
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-01-18 19:44:30 -0500

    funcionan peticiones en postgres !!!!!!!!!!!

--- src/main/java/com/company/busbookingapi/service/BookingServicePostgres.java
+++ src/main/java/com/company/busbookingapi/service/BookingServicePostgres.java
@@ -1,7 +1,43 @@
 package com.company.busbookingapi.service;
 
+import com.company.busbookingapi.BookingService;
+import com.company.busbookingapi.domain.entity.BookingPostgres;
+import com.company.busbookingapi.domain.repository.BookingPostgresRepository;
+import com.company.busbookingapi.mapper.BookingMapperPostgres;
 import org.springframework.stereotype.Service;
+import com.company.busbookingapi.domain.dto.BookingDto;
 
+import java.util.List;
 
-public record BookingServicePostgres() {
+@Service
+public class BookingServicePostgres implements BookingService<BookingDto> {
+    private final BookingPostgresRepository bookingPostgresRepository;
+    private final BookingMapperPostgres mapper;
+
+    public BookingServicePostgres(BookingPostgresRepository bookingPostgresRepository, BookingMapperPostgres mapper) {
+        this.bookingPostgresRepository = bookingPostgresRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void save(BookingDto dto) {
+        BookingPostgres bookingPostgres = mapper.toEntity(dto);
+        bookingPostgresRepository.save(bookingPostgres);
+
+    }
+
+    @Override
+    public BookingDto findById(Object id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<BookingDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Object id) {
+
+    }
 }
